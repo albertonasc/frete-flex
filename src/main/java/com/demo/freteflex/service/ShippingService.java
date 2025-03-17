84325baba1639d7a02adbeec8bd1f9454cffc93b
albertonasc <beto.bdn@gmail.com>
2025-03-17 16:19:23 -0300

    Created Express Shipping Calculator

--- src/main/java/com/demo/freteflex/service/ShippingService.java
+++ src/main/java/com/demo/freteflex/service/ShippingService.java
@@ -1,5 +1,6 @@
 package com.demo.freteflex.service;
 
+import com.demo.freteflex.domain.ExpressShippingCalculator;
 import com.demo.freteflex.domain.StandardShippingCalculator;
 import org.springframework.stereotype.Service;
 
@@ -7,9 +8,11 @@ import org.springframework.stereotype.Service;
 public class ShippingService {
 
     private final StandardShippingCalculator standardShippingCalculator;
+    private final ExpressShippingCalculator expressShippingCalculator;
 
-    public ShippingService(StandardShippingCalculator standardShippingCalculator) {
+    public ShippingService(StandardShippingCalculator standardShippingCalculator, ExpressShippingCalculator expressShippingCalculator) {
         this.standardShippingCalculator = standardShippingCalculator;
+        this.expressShippingCalculator = expressShippingCalculator;
     }
 
     public Double calculate(String shippingTye,
@@ -20,7 +23,11 @@ public class ShippingService {
             return standardShippingCalculator.calculate(distance, weight);
         }
 
-        return 0.0;
+        if(shippingTye.equalsIgnoreCase("express")) {
+            return expressShippingCalculator.calculate(distance, weight);
+        }
+
+        return null;
     }
 
 }
