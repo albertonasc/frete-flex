5f4c2bf77604483b1c1e61f92880d29fe05c688c
albertonasc <beto.bdn@gmail.com>
2025-03-17 15:55:54 -0300

    Created shipping service

--- src/main/java/com/demo/freteflex/controller/ShippingController.java
+++ src/main/java/com/demo/freteflex/controller/ShippingController.java
@@ -1,6 +1,7 @@
 package com.demo.freteflex.controller;
 
 import com.demo.freteflex.controller.dto.ShippingResponse;
+import com.demo.freteflex.service.ShippingService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,16 +10,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ShippingController {
 
+    private final ShippingService shippingService;
+
+    public ShippingController(ShippingService shippingService) {
+        this.shippingService = shippingService;
+    }
+
     @GetMapping(value = "/shipping/calculate")
     public ResponseEntity<ShippingResponse> calculate(@RequestParam("type") String shippingType,
                                                       @RequestParam("distance") Double distance,
                                                       @RequestParam("weight") Double weight
     ) {
 
-        // TODO - calculo do frete
+        var cost = shippingService.calculate(shippingType, distance, weight);
 
-
-        return ResponseEntity.ok(new ShippingResponse(0.0));
+        return ResponseEntity.ok(new ShippingResponse(cost));
     }
 
 
---
+++ src/main/java/com/demo/freteflex/service/ShippingService.java
@@ -0,0 +1,15 @@
+package com.demo.freteflex.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShippingService {
+
+    public Double calculate(String shippingTye,
+                            Double distance,
+                            Double weight) {
+
+        return 0.0;
+    }
+
+}
